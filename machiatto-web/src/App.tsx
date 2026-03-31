ae6cd9d3f1c9b37b66089047f2803bbf67c631e9
SatoshiGhost <walzustore@gmail.com>
2026-03-30 22:19:59 -0600

    Deploy: Add base path /machiatto/, .htaccess SPA fallback, FTP deployed to nymphtech.com/machiatto/

--- machiatto-web/src/App.tsx
+++ machiatto-web/src/App.tsx
@@ -30,7 +30,7 @@ function AnimatedRoutes() {
 
 function App() {
   return (
-    <Router>
+    <Router basename="/machiatto">
       <div className="flex flex-col min-h-screen bg-[#0D0A1F]">
         <Navbar />
         <main className="flex-1">
